be7656049ec1301759264d9b68b3d96869f1ec6f
mendes478 <kawagabi2019@gmail.com>
2025-11-15 20:57:08 -0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+import time
+
+app = Flask(__name__)
+
+@app.route("/process", methods=["POST"])
+def process():
+    data = request.json.get("value", 0)
+
+    # Simulação de processamento pesado (ex.: IA, análise, etc.)
+    time.sleep(0.2)  # 200 ms
+
+    result = data * data  # operação qualquer
+
+    return jsonify({
+        "input": data,
+        "output": result
+    })
+
+if __name__ == "__main__":
+    # Servidor rodando na máquina local (edge)
+    app.run(host="0.0.0.0", port=5000)
